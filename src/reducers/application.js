427770cd3a9c34dc22350991489b8b77fcb5760d
Karl Jensen <karljensen@gmail.com>
2019-08-10 00:38:08 +0000

    Fix the build

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -4,6 +4,8 @@ export const SET_INTERVIEW = "SET_INTERVIEW";
 
 export default function reducer(state, action) {
   switch (action.type) {
+    case SET_DAY:
+      return { ...state, day: action.day };
     case SET_APPLICATION_DATA:
       return {
         ...state,
